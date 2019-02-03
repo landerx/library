02e82a126d86052493e4e922700a055b3eeb260f
Piotr Landzwojczak <piotr.landzwojczak@collibra.com>
2019-02-03 14:29:47 +0100

    login mechanism

---
+++ src/com/craftioncode/library/login/LoginServlet.java
@@ -0,0 +1,40 @@
+package com.craftioncode.library.login;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.craftioncode.library.users.User;
+import com.craftioncode.library.users.dao.UsersDAO;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String login = req.getParameter("login");
+		String password = req.getParameter("password");
+
+		Optional<User> userOpt = UsersDAO.getAll()
+				.stream()
+				.filter(user -> user.getLogin().equals(login) && user.getPassword().equals(password))
+				.findFirst();
+
+		if (userOpt.isPresent()) {
+			User user = userOpt.get();
+			HttpSession session = req.getSession();
+			session.setAttribute("username", user.getLogin());
+			resp.sendRedirect("index.jsp");
+		} else {
+			resp.sendRedirect("login.jsp?invalidLogin");
+		}
+
+
+	}
+}
